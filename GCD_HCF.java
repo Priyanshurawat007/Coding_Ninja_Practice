18a326c23e0a2b26a9c402d7bdfd1584ad9ac3e0
Priyanshu Rawat <100402236+Priyanshurawat007@users.noreply.github.com>
2024-04-10 01:29:00 +0530

    Add files via upload

---
+++ GCD_HCF.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+public class GCD_HCF {
+    
+        public static int calcGCD(int n, int m){
+           
+            while (n>0 && m>0) {
+                if(n>m){
+                    n=n%m;
+                }else{
+                    m=m%n;
+    
+                }
+                
+            }
+            if(n==0){
+                return m;
+            }else{
+                return n;
+            }
+        }
+    }
