4832e543ac4a7975bf9d87868045b13d3d502c7c
Priyanshu Rawat <100402236+Priyanshurawat007@users.noreply.github.com>
2024-06-03 20:51:56 +0530

    Create AddTwoNumers.java

---
+++ AddTwoNumers.java
@@ -0,0 +1,43 @@
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+    class LinkedListNode {
+        int data;
+        LinkedListNode next;
+        
+        public LinkedListNode(int data) {
+            this.data = data;
+        }
+    }
+
+*****************************************************************/
+
+
+public class Solution {
+    static LinkedListNode addTwoNumbers(LinkedListNode head1, LinkedListNode head2) {
+         LinkedListNode dummyNode = new LinkedListNode(0);
+        LinkedListNode p = head1, q = head2, current = dummyNode;
+        int carry = 0;
+        
+        while (p != null || q != null) {
+            int x = (p != null) ? p.data : 0;
+            int y = (q != null) ? q.data : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            current.next = new LinkedListNode(sum % 10);
+            current = current.next;
+            
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        
+        if (carry > 0) {
+            current.next = new LinkedListNode(carry);
+        }
+        
+        return dummyNode.next;
+    }
+}
